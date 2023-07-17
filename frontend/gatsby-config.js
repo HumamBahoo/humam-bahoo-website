2d01b6e274ecad98c07209a61608a3a2c39d9e6a
Humam Bahoo <humam.bahoo@gmail.com>
2023-07-17 04:07:39 -0400

    install and setup gatsby-plugin-image

--- frontend/gatsby-config.js
+++ frontend/gatsby-config.js
@@ -20,6 +20,8 @@ module.exports = {
   plugins: [
     `gatsby-plugin-postcss`,
     `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`, // Needed for dynamic images
     {
       resolve: `gatsby-source-sanity`,
       options: {
