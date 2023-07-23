6a0ba182ad380f90cecea97d9795fa454422b3d8
Humam Bahoo <humam.bahoo@gmail.com>
2023-07-23 15:48:47 -0400

    update homepage

--- frontend/src/pages/index.js
+++ frontend/src/pages/index.js
@@ -12,32 +12,29 @@ const HomePage = ({ data }) => {
 
   return (
     <Layout>
-      <div className="flex flex-col-reverse items-center justify-between gap-12 lg:flex-row">
-        <section className="flex basis-1/2 flex-col items-center gap-6 text-center lg:items-start lg:gap-8 lg:text-left">
-          <p className="xs:text-2xl text-xl">Hello, I'm</p>
+      <section>
+        <div>
+          <h1>
+            Hi, I'm <span>{fullName}</span>
+          </h1>
 
-          <h1 className="xs:text-4xl text-3xl font-black uppercase text-orange-400">{fullName}</h1>
-
-          <p className="text-base lg:text-justify">{introduction}</p>
+          <p>{introduction}</p>
 
           <a
+            className="btn"
             href={resumeUrl}
             target="_blank"
             rel="noopener noreferrer"
-            className="xs:text-base flex flex-row items-center justify-center gap-2 rounded-xl bg-orange-400 px-4 py-2 text-sm font-black uppercase leading-normal text-orange-950 no-underline hover:bg-orange-200 active:bg-orange-300"
           >
-            My Resume <MdOpenInBrowser className="text-2xl" />
+            My Resume <MdOpenInBrowser />
           </a>
-        </section>
+        </div>
 
-        <section className="h-auto max-w-[350px] basis-1/2 lg:max-w-[350px]">
-          <GatsbyImage
-            image={picture}
-            alt={`${fullName} - Photo `}
-            className="mx-auto rounded-full"
-          />
-        </section>
-      </div>
+        <GatsbyImage
+          image={picture}
+          alt={`${fullName} - Photo `}
+        />
+      </section>
     </Layout>
   );
 };
@@ -58,7 +55,7 @@ export const query = graphql`
       }
       picture {
         asset {
-          gatsbyImageData(fit: FILLMAX, placeholder: DOMINANT_COLOR)
+          gatsbyImageData(width: 400, placeholder: BLURRED, formats: [AUTO, WEBP, AVIF])
         }
       }
     }
