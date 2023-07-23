776112662a43335ff989da2599761d638ecb9313
Humam Bahoo <humam.bahoo@gmail.com>
2023-07-23 15:46:58 -0400

    remove extra screen size

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,16 +1,6 @@
-const defaultTheme = require('tailwindcss/defaultTheme');
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./src/pages/**/*.{js,jsx,ts,tsx}', './src/components/**/*.{js,jsx,ts,tsx}'],
-  theme: {
-    extend: {
-      screens: {
-        xs: '400px',
-        // => media (min-width: 400px) { ... }
-        ...defaultTheme.screens,
-      },
-    },
-  },
+  theme: {},
   plugins: [],
 };
