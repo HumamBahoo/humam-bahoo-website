611953549d514a10e6b9b757bab15b7122eb9d59
Humam Bahoo <humam.bahoo@gmail.com>
2023-07-24 16:23:09 -0400

    start portfolio page

---
+++ frontend/src/pages/portfolio/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Layout from '../../components/Layout';
+
+const PortfolioPage = () => {
+  return (
+    <Layout>
+      <div className="py-12">
+        <h1 className=" text-4xl text-orange-400">PortfolioPage</h1>
+        <p className="text-xl">Page in development</p>
+      </div>
+    </Layout>
+  );
+};
+
+export default PortfolioPage;
