5f847478960eeb407c308ffdab0e9e03611b48a5
Humam Bahoo <humam.bahoo@gmail.com>
2023-07-24 16:22:31 -0400

    implement 404 page

--- frontend/src/pages/404.js
+++ frontend/src/pages/404.js
@@ -5,17 +5,18 @@ import Layout from '../components/Layout';
 const NotFoundPage = () => {
   return (
     <Layout>
-      <div className="info">
-        <h1>404: Page Not Found</h1>
-        <p>Sorry, the page you are looking for doesn't exist.</p>
-      </div>
+      <div className="flex flex-col items-center justify-between gap-12 px-12 text-center lg:py-12">
+        <h1 className="text-2xl font-bold text-orange-400 sm:text-4xl">404: Page Not Found</h1>
 
-      <Link
-        className="btn"
-        href="/"
-      >
-        Go Home
-      </Link>
+        <p className="text-xl sm:text-2xl">Sorry, the page you are looking for does not exist.</p>
+
+        <Link
+          to="/"
+          className="flex w-fit flex-row items-center justify-between gap-2 rounded-xl bg-orange-400 px-8 py-2 font-bold uppercase text-stone-900 hover:bg-orange-300"
+        >
+          Go Home
+        </Link>
+      </div>
     </Layout>
   );
 };
